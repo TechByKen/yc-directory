c3c3d59ffebb1a82fafa79e65a1978c8dd32922b
TechByKen <diala.kenneth98@gmail.com>
2025-08-14 03:43:37 +0500

    Added search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
+import SearchForm from "@/app/components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
+ const query = (await searchParams).query;
   return (
     <>
     <section className="w-full bg-pink-500 min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
 
        <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center- my-5">Tone your startup, <br /> Connect with Entrepreneurs</h1>
        <p className="!max-w-3xl font-medium text-[20px] text-white max-w-2xl text-center break-words">Submit Ideas, Vote on Tones, and Get Noticed in Virtual Competitions.</p>
+       <SearchForm query={query}/>
     </section>
     
     
---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,16 @@
+import SearchFormReset from "./SearchFormReset"
+
+
+const SearchForm = ({query}: {query?: string}) => {
+
+  return (
+    <form action="/" className="search-form">
+      <input name="query" defaultValue={query} className="search-input" placeholder="Search Startup"/>
+      <div className="flex gap-2">{query && <SearchFormReset/>}
+      <button type="submit" className="search-btn text-white">S</button>
+      </div>
+    </form>
+  )
+}
+
+export default SearchForm
