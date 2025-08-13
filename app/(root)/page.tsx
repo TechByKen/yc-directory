a54def2fcff9fa893a73d7a3e5aa766582eaed7d
TechByKen <diala.kenneth98@gmail.com>
2025-08-13 14:49:38 +0500

    Worked on the hero section and added pattern

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-5xl">Home page</h1>
-    <div><h2>This is the home page</h2></div>
+    <section className="w-full bg-pink-500 min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+
+       <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center- my-5">Tone your startup, <br /> Connect with Entrepreneurs</h1>
+       <p className="!max-w-3xl font-medium text-[20px] text-white max-w-2xl text-center break-words">Submit Ideas, Vote on Tones, and Get Noticed in Virtual Competitions.</p>
+    </section>
+    
+    
     </>
   );
 }
+
